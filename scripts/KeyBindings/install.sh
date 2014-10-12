7f9e7483c2bb20a29890a90e6ca0752f04e03a50
Michael Sanders <michael@msanders.com>
2014-10-11 17:31:07 -0700

    Apparently DefaultKeyBinding.dict can't be symlinked

---
+++ scripts/KeyBindings/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eu
+
+DIRNAME=$(dirname "$0")
+OUTDIR="$HOME/Library/KeyBindings"
+
+# Apparently DefaultKeyBinding.dict can't be symlinked.
+# See https://apple.stackexchange.com/a/53110
+mkdir -p "$OUTDIR"
+cp -p "$DIRNAME/DefaultKeyBinding.dict" "$OUTDIR"
+echo "Installed key bindings"
