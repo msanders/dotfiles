9383063f649de69458affcb932d87060b6c579f0
Michael Sanders <michael.sanders@fastmail.com>
2019-04-18 18:51:50 -0700

    Update bootstrap & fix node script

--- scripts/node_setup.sh
+++ scripts/node_setup.sh
@@ -2,7 +2,7 @@
 set -o errexit -o pipefail -o nounset
 
 echo "Updating npm dependencies."
+npm install -g npm
 npm install -g elm elm-live elm-format gulp gulp-elm
 npm install -g typescript gulp-typescript tern
 npm install -g ios-deploy
-npm install -g npm
