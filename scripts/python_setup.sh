6e5babfb28611a2e3a7d926f8f8bf3ad6b4738a2
Michael Sanders <michael.sanders@fastmail.com>
2019-04-18 18:51:11 -0700

    Update Python versions

--- scripts/python_setup.sh
+++ scripts/python_setup.sh
@@ -2,18 +2,18 @@
 set -o errexit -o pipefail -o nounset
 
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
-pyenv install 2.7.14 --skip-existing
-pyenv install 3.6.7 --skip-existing
-pyenv install 3.7.1 --skip-existing
+pyenv install 2.7.15 --skip-existing
+pyenv install 3.6.8 --skip-existing
+pyenv install 3.7.3 --skip-existing
 
 install_dependencies() {
     pip install -U pip setuptools flake8 setuptools-rust autoflake hy yapf \
                    virtualenv
 }
 
-pyenv global 2.7.14
+pyenv shell 2.7.15
 install_dependencies
-pyenv global 3.6.7
+pyenv shell 3.6.8
 install_dependencies
-pyenv global 3.7.1
+pyenv global 3.7.3
 install_dependencies
