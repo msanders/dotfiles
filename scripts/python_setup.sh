a642f49db9548aa772d57917b9a1e3b5a0b021d6
Michael Sanders <michael.sanders@fastmail.com>
2019-05-09 06:07:12 -0700

    Update Python setup script

--- scripts/python_setup.sh
+++ scripts/python_setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-set -o errexit -o pipefail -o nounset
+eval "$(pyenv init -)"
+set -o errexit -o pipefail
 
 export PYTHON_CONFIGURE_OPTS="--enable-framework"
-pyenv install 2.7.15 --skip-existing
+pyenv install 2.7.16 --skip-existing
 pyenv install 3.6.8 --skip-existing
 pyenv install 3.7.3 --skip-existing
 
@@ -11,7 +12,7 @@ install_dependencies() {
                    virtualenv
 }
 
-pyenv shell 2.7.15
+pyenv shell 2.7.16
 install_dependencies
 pyenv shell 3.6.8
 install_dependencies
