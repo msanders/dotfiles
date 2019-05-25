fc4d073fbb82eda1ee04b07b7ee23ade37eb9da9
Michael Sanders <michael.sanders@fastmail.com>
2019-05-24 20:23:30 -0400

    Update Emacs setup script

--- scripts/emacs_setup.sh
+++ scripts/emacs_setup.sh
@@ -2,12 +2,19 @@
 set -o errexit -o pipefail -o nounset
 
 EMACS_DIR="$HOME/.emacs.d"
+EMACS_APP_PATH="/usr/local/opt/emacs-mac/Emacs.app"
 
 if [ ! -d "$EMACS_DIR" ]; then
     git clone https://github.com/syl20bnr/spacemacs "$EMACS_DIR"
     echo "Installed Spacemacs."
 fi
 
-if ! which cask >/dev/null; then
+if ! command -v cask >/dev/null; then
     curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+fi
+
+if [ ! -e "/Applications/Emacs" ] && [ ! -e "/Applications/Emacs.app" ]; then
+    osascript -e 'tell application "Finder"' \
+              -e "make alias file to POSIX file \"$EMACS_APP_PATH\" at POSIX file \"/Applications\"" \
+              -e 'end tell' >/dev/null
 fi
