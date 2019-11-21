f5ff97a3371002f729b84c051cb5f2f348c769ca
Michael Sanders <michael.sanders@fastmail.com>
2019-11-21 13:01:37 -0800

    Move to Neovim

--- scripts/vim_setup.sh
+++ scripts/vim_setup.sh
@@ -8,4 +8,4 @@ if [ ! -d "$VIM_DIR/bundle/Vundle.vim" ]; then
     git clone https://github.com/gmarik/Vundle.vim.git "$VIM_DIR/bundle/Vundle.vim"
 fi
 
-vim +PluginUpdate +qall
+nvim +PluginUpdate +qall
