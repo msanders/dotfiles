089fd36eae72880766ca816a73286576ed6fa45f
Michael Sanders <michael.sanders@fastmail.com>
2019-11-21 12:58:17 -0800

    Update Python versions

--- scripts/python_setup.sh
+++ scripts/python_setup.sh
@@ -8,18 +8,21 @@ else
     export PYTHON_CONFIGURE_OPTS="--enable-shared"
 fi
 
-pyenv install 2.7.16 --skip-existing
-pyenv install 3.6.8 --skip-existing
-pyenv install 3.7.3 --skip-existing
+pyenv install 2.7.17 --skip-existing
+pyenv install 3.6.9 --skip-existing
+pyenv install 3.7.5 --skip-existing
+pyenv install 3.8.0 --skip-existing
 
 install_dependencies() {
     pip install -U pip setuptools flake8 setuptools-rust autoflake hy yapf \
                    virtualenv pylint
 }
 
-pyenv shell 2.7.16
+pyenv shell 2.7.17
 install_dependencies
-pyenv shell 3.6.8
+pyenv shell 3.6.9
 install_dependencies
-pyenv global 3.7.3
+pyenv shell 3.7.5
+install_dependencies
+pyenv global 3.8.0
 install_dependencies
