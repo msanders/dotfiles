09fc90ebe365b2238c1fe436a66445044341d3cb
Michael Sanders <michael.sanders@fastmail.com>
2019-12-28 18:52:30 -0800

    Add & run shfmt

--- run/before/fish_setup.sh
+++ run/before/fish_setup.sh
@@ -5,7 +5,7 @@ OMF_DOWNLOAD_DIR="$(mktemp -d -t ohmyfish)"
 (
     cd "$OMF_DOWNLOAD_DIR"
     echo "Downloading Oh My Fish..."
-    curl --progress-bar -L https://get.oh-my.fish > install
+    curl --progress-bar -L https://get.oh-my.fish >install
     echo "Installing..."
     NONINTERACTIVE=true fish ./install --path=~/.local/share/omf --config=~/.config/omf --yes
     rm -rf "$OMF_DOWNLOAD_DIR"
